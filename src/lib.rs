e296b130768685599bb969a2e15ff01eb4b8e7e2
JuEnPeHa <jeph@nearlearning.app>
2022-04-07 17:40:12 -0500

    0001

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen, setup_alloc};
 
+setup_alloc!();
 
+//Function accept offer
+
+//Function last offer
+
+//Function create bid
+
+//Function put offer
+
+//Function transfer Paras
+
+//Function transfer nft Mintbase
+
+//Function is owner
+
+//Function is still owner
 
 // #[cfg(test)]
 // mod tests {
